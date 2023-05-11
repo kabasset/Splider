b767150f07ccad9025017ed77a6ddd8ec5a7778a
Antoine Basset <antoine.basser@cnes.fr>
2023-05-11 13:41:50 +0000

    WIP: Rely on fixtures

--- Splider/tests/src/Spline_test.cpp
+++ Splider/tests/src/Spline_test.cpp
@@ -25,19 +25,29 @@ BOOST_AUTO_TEST_CASE(index_test) {
   BOOST_CHECK_THROW(u.index(u[u.size() - 1] + 1), std::runtime_error);
 }
 
-BOOST_AUTO_TEST_CASE(real_spline_test) {
-  const Splider::SplineIntervals u {1, 2, 3, 4};
-  const std::vector<double> x {1.1, 2.5, 3.9};
-  const std::vector<double> v {10, 20, 30, 40};
-  const Splider::Spline<double> spline(u, v);
-  std::vector<double> y;
+template <typename X, typename Y>
+struct LinSplineFixture {
+  std::vector<X> u {1, 2, 3, 4};
+  Splider::SplineIntervals domain = Splider::SplineIntervals(u);
+  Splider::SplineBuilder builder = Splider::SplineBuilder(u);
+  std::vector<X> x {1.1, 2.5, 3.9};
+  std::vector<Y> v {10, 20, 30, 40};
+  std::vector<Y> y {11, 25, 39};
+};
+
+using RealLinSplineFixture = LinSplineFixture<double, double>;
+using ComplexLinSplineFixture = LinSplineFixture<double, std::complex<double>>;
+
+BOOST_FIXTURE_TEST_CASE(real_spline_test, RealLinSplineFixture) {
+  const Splider::Spline<double> spline(domain, v);
+  std::vector<double> out;
   for (const auto& e : x) {
-    y.push_back(spline(e));
+    out.push_back(spline(e));
   }
-  BOOST_TEST(y.size() == x.size());
-  for (std::size_t i = 0; i < y.size(); ++i) {
-    BOOST_TEST(y[i] > v[i]);
-    BOOST_TEST(y[i] < v[i + 1]);
+  BOOST_TEST(out.size() == x.size());
+  for (std::size_t i = 0; i < out.size(); ++i) {
+    BOOST_TEST(out[i] > v[i]);
+    BOOST_TEST(out[i] < v[i + 1]);
   }
 }
 
