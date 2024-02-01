021b898864cdc92c680651d6069f2db32ac49d81
Antoine Basset <antoine.basset@cnes.fr>
2024-02-01 09:38:38 +0000

    Optimize and simplify design

--- Splider/Splider/Natural.h
+++ Splider/Splider/Natural.h
@@ -13,62 +13,74 @@ namespace Splider {
  * 
  * This is the only C2 cubic spline, which comes at the cost of more computing.
  */
-class Natural : public C2Mixin<Natural> {
+class Natural : public BuilderMixin<Natural> {
 
 public:
   /**
-   * @brief Solve the tridiagonal system using Thomas algorithm.
+   * @brief The argument type.
    */
-  template <typename TSpline>
-  static void update(TSpline& params, Linx::Index) {
-    if (params.m_valid) {
-      return;
+  template <typename TDomain>
+  using Arg = C2Arg<TDomain>;
+
+  /**
+   * @brief The spline evaluator.
+   */
+  template <typename TDomain, typename T>
+  class Spline : public C2SplineMixin<TDomain, T, Spline<TDomain, T>> {
+    using Mixin = C2SplineMixin<TDomain, T, Spline>;
+
+  public:
+    template <typename... TParams>
+    Spline(TParams&&... params) : Mixin(LINX_FORWARD(params)...) {}
+
+    /**
+     * @brief Solve the tridiagonal system using Thomas algorithm.
+     */
+    void update(Linx::Index) {
+      if (Mixin::m_valid) {
+        return;
+      }
+
+      const Linx::Index n = this->m_s.size();
+      std::vector<typename Mixin::Real> diag(n);
+      std::vector<typename Mixin::Value> rhs(n);
+
+      // Initialize i = 1 for merging initialization and forward pass
+      auto h0 = this->m_domain.length(0);
+      auto h1 = this->m_domain.length(1);
+      auto dv0 = (this->m_v[1] - this->m_v[0]) / h0;
+      auto dv1 = (this->m_v[2] - this->m_v[1]) / h1;
+      diag[1] = 2. * (h0 + h1);
+      rhs[1] = 6. * (dv1 - dv0);
+
+      // Initialization and forward pass
+      for (Linx::Index i = 2; i < n - 1; ++i) {
+        h0 = h1;
+        h1 = this->m_domain.length(i);
+        dv0 = dv1;
+        dv1 = (this->m_v[i + 1] - this->m_v[i]) / h1;
+        const auto w = h1 / diag[i - 1];
+        diag[i] = 2. * (h0 + h1) - w * h1;
+        rhs[i] = 6. * (dv1 - dv0) - w * rhs[i - 1];
+      }
+
+      // Backward pass
+      this->m_s[n - 2] = rhs[n - 2] / diag[n - 2];
+      for (auto i = n - 3; i > 0; --i) {
+        this->m_s[i] = (rhs[i] - this->m_domain.length(i) * this->m_s[i + 1]) / diag[i];
+      }
+
+      // Natutal spline // FIXME useful?
+      this->m_s[0] = 0;
+      this->m_s[n - 1] = 0;
+
+      this->m_valid = true;
     }
-
-    using Real = typename TSpline::Real;
-    using Value = typename TSpline::Value;
-    const auto& domain = params.m_domain;
-    const auto& v = params.m_v;
-    auto& s = params.m_s;
-    const Linx::Index n = s.size();
-    std::vector<Real> b(n);
-    std::vector<Value> d(n);
-
-    // Initialize i = 1 for merging initialization and forward pass
-    auto h0 = domain.length(0);
-    auto h1 = domain.length(1);
-    auto dv0 = (v[1] - v[0]) / h0;
-    auto dv1 = (v[2] - v[1]) / h1;
-    b[1] = 2. * (h0 + h1);
-    d[1] = 6. * (dv1 - dv0);
-
-    // Initialization and forward pass
-    for (Linx::Index i = 2; i < n - 1; ++i) {
-      h0 = h1;
-      h1 = domain.length(i);
-      dv0 = dv1;
-      dv1 = (v[i + 1] - v[i]) / h1;
-      const auto w = h1 / b[i - 1];
-      b[i] = 2. * (h0 + h1) - w * h1;
-      d[i] = 6. * (dv1 - dv0) - w * d[i - 1];
-    }
-
-    // Backward pass
-    s[n - 2] = d[n - 2] / b[n - 2];
-    for (auto i = n - 3; i > 0; --i) {
-      s[i] = (d[i] - domain.length(i) * s[i + 1]) / b[i];
-    }
-
-    // Natutal spline // FIXME useful?
-    s[0] = 0;
-    s[n - 1] = 0;
-
-    params.m_valid = true;
-  }
+  };
 };
 
 namespace FiniteDiff {
-class Natural : public C2Mixin<FiniteDiff::Natural> {};
+class Natural : public BuilderMixin<FiniteDiff::Natural> {};
 } // namespace FiniteDiff
 
 } // namespace Splider
--- Splider/Splider/mixins/C2.h
+++ Splider/Splider/mixins/C2.h
@@ -11,189 +11,180 @@
 namespace Splider {
 
 /**
- * @brief Mixin for C2 splines.
+ * @brief An argument.
  */
-template <typename TDerived>
-class C2Mixin : public BuilderMixin<TDerived> {
+template <typename TDomain>
+class C2Arg {
+  template <typename, typename, typename>
+  friend class C2SplineMixin;
 
 public:
   /**
-   * @brief An argument.
+   * @brief The domain type.
    */
-  template <typename TDomain>
-  class Arg {
-    template <typename, typename>
-    friend class TDerived::Spline;
-
-  public:
-    /**
-     * @brief The domain type.
-     */
-    using Domain = TDomain;
-
-    /**
-     * @brief The argument floating point type.
-     */
-    using Real = typename Domain::Value;
-
-    /**
-     * @brief Constructor.
-     */
-    explicit Arg(const Domain& domain, Real x) {
-      m_i = domain.index(x);
-      const auto h = domain.length(m_i);
-      const auto left = x - domain[m_i];
-      const auto right = h - left;
-      m_cv0 = right / h;
-      m_cv1 = 1 - m_cv0;
-      m_cs0 = right / 6. * (right * m_cv0 - h);
-      m_cs1 = left / 6. * (left * m_cv1 - h);
-      // FIXME rm 6 ?
-    }
-
-  private:
-    Linx::Index m_i; ///< The subinterval index
-    Real m_cv0; ///< The `v[i]` coefficient
-    Real m_cv1; ///< The `v[i + 1]` coefficient
-    Real m_cs0; ///< The `s[i]` coefficient
-    Real m_cs1; ///< The `s[i + 1]` coefficient
-  };
+  using Domain = TDomain;
 
   /**
-   * @brief The parameters.
+   * @brief The argument floating point type.
    */
-  template <typename TDomain, typename T>
-  class Spline { // FIXME Mixin for operator()?
-    friend class TDerived::Method;
+  using Real = typename Domain::Value;
 
-  public:
-    /**
-     * @brief The knots domain.
-     */
-    using Domain = TDomain;
+  /**
+   * @brief Constructor.
+   */
+  explicit C2Arg(const Domain& domain, Real x) {
+    m_i = domain.index(x);
+    const auto h = domain.length(m_i);
+    const auto left = x - domain[m_i];
+    const auto right = h - left;
+    m_cv0 = right / h;
+    m_cv1 = 1 - m_cv0;
+    m_cs0 = right / 6. * (right * m_cv0 - h);
+    m_cs1 = left / 6. * (left * m_cv1 - h);
+    // FIXME rm 6 ?
+  }
 
-    /**
+private:
+  Linx::Index m_i; ///< The subinterval index
+  Real m_cv0; ///< The `v[i]` coefficient
+  Real m_cv1; ///< The `v[i + 1]` coefficient
+  Real m_cs0; ///< The `s[i]` coefficient
+  Real m_cs1; ///< The `s[i + 1]` coefficient
+};
+
+/**
+ * @brief The parameters.
+ */
+template <typename TDomain, typename T, typename TDerived>
+class C2SplineMixin { // FIXME Upper mixin for operator()?
+
+public:
+  /**
+   * @brief The knots domain.
+   */
+  using Domain = TDomain;
+
+  /**
    * @brief The abscissae floating point type.
-     */
-    using Real = typename Domain::Value;
+   */
+  using Real = typename Domain::Value;
 
-    /**
-     * @brief The argument type.
-     */
-    using Arg = typename TDerived::Arg<Domain>;
+  /**
+   * @brief The argument type.
+   */
+  using Arg = C2Arg<Domain>;
 
-    /**
-     * @brief The knot value type.
-     */
-    using Value = T;
+  /**
+   * @brief The knot value type.
+   */
+  using Value = T;
 
-    /**
-     * @brief Null knots constructor.
-     */
-    explicit Spline(const Domain& u) : m_domain(u), m_v(m_domain.size()), m_s(m_domain.size()), m_valid(true) {}
+  /**
+   * @brief Null knots constructor.
+   */
+  explicit C2SplineMixin(const Domain& u) : m_domain(u), m_v(m_domain.size()), m_s(m_domain.size()), m_valid(true) {}
 
-    /**
-     * @brief Iterator-based constructor.
-     */
-    template <typename TIt>
-    explicit Spline(const Domain& u, TIt begin, TIt end) :
-        m_domain(u), m_v(begin, end), m_s(m_v.size()), m_valid(false) {}
+  /**
+   * @brief Iterator-based constructor.
+   */
+  template <typename TIt>
+  explicit C2SplineMixin(const Domain& u, TIt begin, TIt end) :
+      m_domain(u), m_v(begin, end), m_s(m_v.size()), m_valid(false) {}
 
-    /**
+  /**
    * @brief Range-based constructor.
    */
-    template <typename TV>
-    explicit Spline(const Domain& u, const TV& v) : Spline(u, std::begin(v), std::end(v)) {}
+  template <typename TV>
+  explicit C2SplineMixin(const Domain& u, const TV& v) : C2SplineMixin(u, std::begin(v), std::end(v)) {}
 
-    /**
+  /**
    * @brief List-based constructor.
    */
-    template <typename TV>
-    explicit Spline(const Domain& u, std::initializer_list<TV> v) : Spline(u, v.begin(), v.end()) {}
+  template <typename TV>
+  explicit C2SplineMixin(const Domain& u, std::initializer_list<TV> v) : C2SplineMixin(u, v.begin(), v.end()) {}
 
-    /**
-     * @brief Get the knots domain.
-     */
-    inline const Domain& domain() const {
-      return m_domain;
+  /**
+   * @brief Get the knots domain.
+   */
+  inline const Domain& domain() const {
+    return m_domain;
+  }
+
+  /**
+   * @brief Assign the knot values.
+   */
+  template <typename TIt>
+  void assign(TIt begin, TIt end) {
+    m_v.assign(begin, end);
+    m_valid = false;
+  }
+
+  /**
+   * @brief Assign the knot values.
+   */
+  template <typename TV>
+  void assign(const TV& v) {
+    assign(std::begin(v), std::end(v));
+  }
+
+  /**
+   * @brief Assign the knot values.
+   */
+  template <typename TV>
+  void assign(std::initializer_list<TV> v) {
+    assign(v.begin(), v.end());
+  }
+
+  /**
+   * @brief Evaluate the spline for a given argument.
+   */
+  inline Value operator()(Real x) {
+    return operator()(Arg(m_domain, x));
+  }
+
+  /**
+   * @brief Evaluate the spline for a given argument.
+   */
+  Value operator()(const Arg& arg) {
+    const auto i = arg.m_i;
+    static_cast<TDerived&>(*this).update(i);
+    return m_v[i] * arg.m_cv0 + m_v[i + 1] * arg.m_cv1 + m_s[i] * arg.m_cs0 + m_s[i + 1] * arg.m_cs1;
+  }
+
+  /**
+   * @brief Evaluate the spline for multiple arguments.
+   */
+  template <typename TIt>
+  std::vector<Value> operator()(TIt begin, TIt end) {
+    std::vector<Value> out;
+    out.reserve(std::distance(begin, end));
+    for (; begin != end; ++begin) {
+      out.push_back(operator()(*begin));
     }
+    return out;
+  }
 
-    /**
-     * @brief Assign the knot values.
-     */
-    template <typename TIt>
-    void assign(TIt begin, TIt end) {
-      m_v.assign(begin, end);
-      m_valid = false;
-    }
+  /**
+   * @brief Evaluate the spline for multiple arguments.
+   */
+  template <typename TX>
+  std::vector<Value> operator()(const TX& x) {
+    return operator()(std::begin(x), std::end(x));
+  }
 
-    /**
-     * @brief Assign the knot values.
-     */
-    template <typename TV>
-    void assign(const TV& v) {
-      assign(std::begin(v), std::end(v));
-    }
+  /**
+   * @brief Evaluate the spline for multiple arguments.
+   */
+  template <typename TX>
+  std::vector<Value> operator()(std::initializer_list<TX> x) {
+    return operator()(x.begin(), x.end());
+  }
 
-    /**
-     * @brief Assign the knot values.
-     */
-    template <typename TV>
-    void assign(std::initializer_list<TV> v) {
-      assign(v.begin(), v.end());
-    }
-
-    /**
-     * @brief Evaluate the spline for a given argument.
-     */
-    inline Value operator()(Real x) {
-      return operator()(Arg(m_domain, x));
-    }
-
-    /**
-     * @brief Evaluate the spline for a given argument.
-     */
-    Value operator()(const Arg& arg) {
-      const auto i = arg.m_i;
-      TDerived::update(*this, i);
-      return m_v[i] * arg.m_cv0 + m_v[i + 1] * arg.m_cv1 + m_s[i] * arg.m_cs0 + m_s[i + 1] * arg.m_cs1;
-    }
-
-    /**
-     * @brief Evaluate the spline for multiple arguments.
-     */
-    template <typename TIt>
-    std::vector<Value> operator()(TIt begin, TIt end) {
-      std::vector<Value> out;
-      out.reserve(std::distance(begin, end));
-      for (; begin != end; ++begin) {
-        out.push_back(operator()(*begin));
-      }
-      return out;
-    }
-
-    /**
-     * @brief Evaluate the spline for multiple arguments.
-     */
-    template <typename TX>
-    std::vector<Value> operator()(const TX& x) {
-      return operator()(std::begin(x), std::end(x));
-    }
-
-    /**
-     * @brief Evaluate the spline for multiple arguments.
-     */
-    template <typename TX>
-    std::vector<Value> operator()(std::initializer_list<TX> x) {
-      return operator()(x.begin(), x.end());
-    }
-
-  private:
-    const Domain& m_domain; ///< The knots domain
-    std::vector<Value> m_v; ///< The knot values
-    std::vector<Value> m_s; ///< The knot second derivatives
-    bool m_valid; ///< Validity flag // FIXME as std::set
-  };
+protected:
+  const Domain& m_domain; ///< The knots domain
+  std::vector<Value> m_v; ///< The knot values
+  std::vector<Value> m_s; ///< The knot second derivatives
+  bool m_valid; ///< Validity flag // FIXME to TDerived
 };
 
 } // namespace Splider
