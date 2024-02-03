b8a2356a0b6989727a79909adb74661e0a40a4d6
Antoine Basset <antoine.basset@cnes.fr>
2024-02-03 13:35:45 +0000

    Compare all builders at once

--- SpliderRun/src/program/SpliderSin.cpp
+++ SpliderRun/src/program/SpliderSin.cpp
@@ -9,27 +9,7 @@
 #include "Splider/Hermite.h"
 #include "Splider/Lagrange.h"
 
-#include <gsl/gsl_interp.h>
-#include <gsl/gsl_spline.h>
-
-static Elements::Logging logger = Elements::Logging::getLogger("SpliderSin");
-
 using Duration = std::chrono::milliseconds;
-
-template <typename U, typename V, typename X>
-std::vector<double> resample_with_gsl(const U& u, const V& v, const X& x)
-{
-  gsl_interp_accel* acc = gsl_interp_accel_alloc();
-  gsl_spline* spline = gsl_spline_alloc(gsl_interp_cspline, u.size());
-  std::vector<double> y;
-  gsl_spline_init(spline, u.data(), v.data(), u.size());
-  for (const auto& e : x) {
-    y.push_back(gsl_spline_eval(spline, e, acc));
-  }
-  gsl_interp_accel_free(acc);
-  gsl_spline_free(spline);
-  return y;
-}
 
 class SpliderBenchmark : public Elements::Program {
 public:
@@ -39,7 +19,6 @@ public:
     Linx::ProgramOptions options;
     options.positional("knots", "Number of knots", 101L);
     options.positional("args", "Number of arguments", 1001L);
-    options.positional("case", "The test case", std::string("c2"));
     return options.as_pair();
   }
 
@@ -47,49 +26,39 @@ public:
   {
     const auto knot_count = args["knots"].as<Linx::Index>();
     const auto arg_count = args["args"].as<Linx::Index>();
-    const auto setup = args["case"].as<std::string>();
 
-    logger.info();
-    logger.info("Generating knots...");
-    const auto u = Linx::Sequence<double>(knot_count).linspace(0, Linx::pi<double>() * 4).pow(2);
+    std::cout << "Generating knots..." << std::endl;
+    const auto u = Linx::Sequence<double>(knot_count).linspace(0, Linx::pi<double>() * 2.5);
     const auto x = Linx::Sequence<double>(arg_count).linspace(0, max(u));
     const auto v = sin(u);
     const auto gt = sin(x);
 
-    logger.info("i\tu\tv");
+    std::cout << "i\tu\tv" << std::endl;
     for (std::size_t i = 0; i < u.size(); ++i) {
-      logger.info() << i << '\t' << u[i] << '\t' << v[i];
+      std::cout << i << '\t' << u[i] << '\t' << v[i] << std::endl;
     }
 
-    logger.info();
-    logger.info("Interpolating...");
-    std::vector<double> y;
-    if (setup == "c2") {
-      const auto build = Splider::C2::builder(u);
-      auto cospline = build.cospline(x);
-      y = cospline(v);
-    } else if (setup == "c2fd") {
-      const auto build = Splider::C2::FiniteDiff::builder(u);
-      auto cospline = build.cospline(x);
-      y = cospline(v);
-    } else if (setup == "hermite") {
-      const auto build = Splider::Hermite::FiniteDiff::builder(u);
-      auto cospline = build.cospline(x);
-      y = cospline(v);
-    } else if (setup == "cr") {
-      const auto build = Splider::Hermite::CatmullRom::Uniform::builder(u);
-      auto cospline = build.cospline(x);
-      y = cospline(v);
-    } else if (setup == "lagrange") {
-      const auto build = Splider::Lagrange::builder(u);
-      auto cospline = build.cospline(x);
-      y = cospline(v);
-    }
-    const auto gsl = resample_with_gsl(u, v, x);
+    std::cout << "Interpolating..." << std::endl;
+    std::vector<std::vector<double>> y;
+    std::cout << "i\tx\tsin(x)";
+    std::cout << "\tC2";
+    y.push_back(Splider::C2::eval(u, v, x));
+    std::cout << "\tC2 FD";
+    y.push_back(Splider::C2::FiniteDiff::eval(u, v, x));
+    std::cout << "\tHermite FD";
+    y.push_back(Splider::Hermite::FiniteDiff::eval(u, v, x));
+    std::cout << "\tCatmull-Rom";
+    y.push_back(Splider::Hermite::CatmullRom::Uniform::eval(u, v, x));
+    std::cout << "\tLagrange";
+    y.push_back(Splider::Lagrange::eval(u, v, x));
+    std::cout << std::endl;
 
-    logger.info("i\tx\tsin(x)\tSplider\tGSL");
     for (std::size_t i = 0; i < x.size(); ++i) {
-      logger.info() << i << '\t' << x[i] << '\t' << gt[i] << '\t' << y[i] << '\t' << gsl[i];
+      std::cout << i << '\t' << x[i] << '\t' << gt[i];
+      for (std::size_t s = 0; s < y.size(); ++s) {
+        std::cout << '\t' << y[s][i];
+      }
+      std::cout << std::endl;
     }
 
     return ExitCode::OK;
